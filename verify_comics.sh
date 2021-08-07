c69e33565da69e00bb457c7615af2046063b2863
Christoffer Green <christoffer.green@gmail.com>
2021-08-07 12:36:31 +0200

    Add skip image verification option
    Refactor to use better menu system and use arithmetic
    context for ifs.

--- verify_comics.sh
+++ verify_comics.sh
@@ -3,11 +3,15 @@
 
 usage()
 {
-  echo "Usage: verify_manga [ -d | --delete ] [ -v | --verbose ] filenames/directories"
+  echo "Usage: verify_manga [ -d | --delete ] [ -v | --verbose ] [ -i | --skip-images ] [ -h | --help ] filenames/directories
+          Delete:      Remove a comic arhive if it is corrupt.
+          Verbose:     Print results from unarchive and image verification tools.
+          Skip-images: Skip verification of images in arhive, only verify the archive compression.
+          Help:        Show this text."
   exit 0
 }
 
-PARSED_ARGUMENTS=$(getopt -a -n verify_comics -o dv --long delete,verbose -- "$@")
+PARSED_ARGUMENTS=$(getopt -a -n verify_comics -o dvih --long delete,verbose,skip-images,help -- "$@")
 VALID_ARGUMENTS=$?
 if [ "$VALID_ARGUMENTS" != "0" ]; then
   usage
@@ -17,16 +21,16 @@ eval set -- "$PARSED_ARGUMENTS"
 while :
 do
   case "$1" in
-    -d | --delete)  DELETE=1     ; shift   ;;
-    -v | --verbose) VERBOSE=1    ; shift   ;;
+    -d | --delete)      DELETE=1      ; shift ;;
+    -v | --verbose)     VERBOSE=1     ; shift ;;
+    -i | --skip-images) SKIP_IMAGES=1 ; shift ;;
+    -h | --help)        HELP=1        ; shift ;;
     --) shift; break ;;
   esac
 done
 
 info () { local TEXT=$1
-  if (( VERBOSE == 1 )); then
-    echo -ne "$TEXT\033[0K\r"
-  fi
+  echo -ne "$TEXT\033[0K\r"
 }
 
 prepare_tmp () {
@@ -36,11 +40,15 @@ prepare_tmp () {
 }
 
 check_return () { local COMMAND="$1"; local CBZ_PATH="$2"; local IMAGE_NAME="$3"
-  eval "$COMMAND" &> /dev/null
+  if (( VERBOSE == 1 )); then
+    eval "$COMMAND"
+  else
+    eval "$COMMAND" &> /dev/null
+  fi
   local STATUS=$?
   if (( STATUS != 0 )); then
     echo "Broken: " $CBZ_PATH $IMAGE_NAME >&2
-    if (( $DELETE == 1 )); then
+    if (( DELETE == 1)); then
       echo "Deleting file: $CBZ_PATH"
       rm "$CBZ_PATH"
     fi
@@ -54,7 +62,7 @@ handle_cbz () { local CBZ_PATH="$1";
   prepare_tmp
   check_return "unzip \"$CBZ_PATH\"" "$CBZ_PATH" "N/A"
   local STATUS=$?
-  if (( $STATUS != 0 )); then
+  if (( STATUS != 0 )) ||  (( SKIP_IMAGES == 1 )); then
     return $STATUS
   fi
   shopt -s nullglob
@@ -74,6 +82,10 @@ parse_dir () { local DIR_PATH=$1
 }
 
 main () {
+  if (( HELP == 1 )); then
+    usage
+    exit 0
+  fi
   for IN_PATH in "$@"; do
     if [[ -d "${IN_PATH}" ]]; then
       parse_dir "${IN_PATH}"
